761a06316c4a7d785d3eeace90523140c4c179cc
emanuellycorreiarocha-del <emanuelly.correia.rocha@escola.pr.gov.br>
2026-04-14 17:36:45 +0000

    v1 git push git push git commit -m v1

---
+++ nivel.js
@@ -0,0 +1,8 @@
+function subirDeNivel(nivelAtual){
+    return nivelAtual + 1;
+};
+
+let meuNivel = 10;
+let nivelAtualizado = subirDeNivel(meuNivel);
+
+console.log("Seu nivel atual é:" + nivelAtualizado);
